0091084a3c8db2dc63b80e6e0aac77861bfa9b31
devrangel <matheusrangel.dev@gmail.com>
2021-04-08 14:20:21 -0300

    Added Selled details and eager loading

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -2,6 +2,7 @@
 using SalesWebMVC.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMVC.Services
 {
@@ -27,7 +28,7 @@ namespace SalesWebMVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(seller => seller.Id == id);
+            return _context.Seller.Include(department => department.Department).FirstOrDefault(seller => seller.Id == id);
         }
 
         public void Remove(int id)
